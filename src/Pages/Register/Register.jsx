c149ae93f10a7e781355c6c1f268ce98a9fb40f0
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-11 00:37:31 +0530

    install lottie react and use a lottie animation on register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,13 @@
+import Lottie from "lottie-react";
+import registerLottie from '../../assets/lottie/register.json'
+
 const Register = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
+        <div className="text-center lg:text-left w-96">
          
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+         <Lottie animationData={registerLottie}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl pl-6">
         <h1 className="text-5xl font-bold ">Register now!</h1>
