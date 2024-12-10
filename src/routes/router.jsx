2713b5a4a7f2a92c760663b9ae92cb1ee44e0661
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-11 00:24:41 +0530

    Mainlayout and Home and register router setup

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,30 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      errorElement:<h2>Page Not Found</h2>,
+      children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        },
+        {
+            path:'/register',
+            element:<Register></Register>
+        },
+        {
+            path:"/login",
+            element:<Login></Login>
+        }
+      ]
     },
   ]);
 
