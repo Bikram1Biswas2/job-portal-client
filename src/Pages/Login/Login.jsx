43878df1040e0b96e2455d7e95e3e8b6990e048a
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-11 12:06:36 +0530

    create signInUSer in AuthProvider and use it in Login component successfully

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,29 +1,38 @@
 import Lottie from "lottie-react";
-import loginLottie from '../../assets/lottie/login.json'
-
+import loginLottie from "../../assets/lottie/login.json";
+import { useContext } from "react";
+import AuthContext from "../../Context/AuthContext";
 
 const Login = () => {
+  const { signInUser } = useContext(AuthContext);
 
-    const handleSignIn = e =>{
-        e.preventDefault()
+  const handleSignIn = (e) => {
+    e.preventDefault();
 
-        const form = e.target 
-        const email = form.email.value 
-        const password = form.password.value 
-        console.log(email,password);
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
+    signInUser(email, password)
+    .then((result) => {
+      console.log(result.user);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+  };
 
-    }
+ 
 
-    return (
-        <div className="hero bg-base-200 min-h-screen">
+  return (
+    <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left w-96">
-         
-         <Lottie animationData={loginLottie}></Lottie>
+          <Lottie animationData={loginLottie}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl pl-6">
-        <h1 className="text-5xl font-bold ">Login now!</h1>
+          <h1 className="text-5xl font-bold ">Login now!</h1>
           <form onSubmit={handleSignIn} className="card-body">
             <div className="form-control">
               <label className="label">
@@ -61,7 +70,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Login;
