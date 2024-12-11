bf0591f5db084f8f85ae3eeda8d7033ad512cc9f
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-11 12:00:11 +0530

    setup login form and login Lottie

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,66 @@
+import Lottie from "lottie-react";
+import loginLottie from '../../assets/lottie/login.json'
 
 
 const Login = () => {
+
+    const handleSignIn = e =>{
+        e.preventDefault()
+
+        const form = e.target 
+        const email = form.email.value 
+        const password = form.password.value 
+        console.log(email,password);
+
+
+    }
+
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left w-96">
+         
+         <Lottie animationData={loginLottie}></Lottie>
         </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl pl-6">
+        <h1 className="text-5xl font-bold ">Login now!</h1>
+          <form onSubmit={handleSignIn} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Sign In</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
     );
 };
 
